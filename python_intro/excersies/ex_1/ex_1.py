47685a6f60ea6d6a210871166db811c924ab4499
gulfa <gunnarroe@gmail.com>
2018-04-05 15:51:40 +0300

    Added non-jupyter version of excersies

---
+++ python_intro/excersies/ex_1/ex_1.py
@@ -0,0 +1,12 @@
+def reverse_string(string):
+    """Reverses the string"""
+    result = ""
+    # your code goes here
+
+    return result
+
+
+if "noitcunf" == reverse_string("function"):
+    print("Excersice solved (at least for this input. You should check you function works for other values too)")
+else:
+    print("Not working")
---
+++ python_intro/excersies/ex_3/ex_3.py
@@ -0,0 +1,14 @@
+def html_table(header, lists):
+    table = ""
+    # Your code in here
+    
+    return table
+
+
+# A simple test
+
+lists = [{"a": 1, "b": 2}, {"a":3, "b": 4}]
+
+table = html_table(["a", "b"], lists)
+
+print(table)
