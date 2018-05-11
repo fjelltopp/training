2a41e95b02f7a786c93642b981c7fb964b23fd8e
Mixmixmix <mikolaj.kundegorski@gmail.com>
2018-05-11 09:05:38 +0300

    making life easier...

--- data_storage_solutions/notebook/docker-commands.sh
+++ data_storage_solutions/notebook/docker-commands.sh
@@ -1,2 +1,2 @@
 docker build -t sql-ex .
-docker run -it --rm -p 8888:8888 --network host -v /home/jyri/meerkat-code/training/data_storage_solutions/notebook/excersises:/home/jovyan/work sql-ex
+docker run -it --rm -p 8888:8888 --network host -v excersises:/home/jovyan/work sql-ex
